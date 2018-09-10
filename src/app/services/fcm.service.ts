90b08dd7ebe89426d1a4fac62e791e77f145ea98
codediodeio <hello@codediode.io>
2018-09-10 15:40:49 -0700

    refactor: move fcm code to service

---
+++ src/app/services/fcm.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { AngularFireMessaging } from '@angular/fire/messaging';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { ToastController } from '@ionic/angular';
+import { tap, switchMap } from 'rxjs/operators';
+
+// Fixing temporary bug in AngularFire
+import * as app from 'firebase';
+const _messaging = app.messaging();
+_messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
+_messaging.onMessage = _messaging.onMessage.bind(_messaging);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FcmService {
+  token;
+
+  constructor(
+    private afMessaging: AngularFireMessaging,
+    private fun: AngularFireFunctions,
+    private toastController: ToastController
+  ) {
+    this.getPermission()
+      .pipe(switchMap(token => this.showMessages()))
+      .subscribe();
+  }
+
+  async makeToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 5000,
+      position: 'top',
+      showCloseButton: true,
+      closeButtonText: 'dismiss'
+    });
+    toast.present();
+  }
+
+  getPermission() {
+    return this.afMessaging.requestToken.pipe(
+      tap(token => (this.token = token))
+    );
+  }
+
+  showMessages() {
+    return this.afMessaging.messages.pipe(
+      tap(msg => {
+        const body: any = (msg as any).notification.body;
+        this.makeToast(body);
+      })
+    );
+  }
+
+  sub(topic) {
+    this.fun
+      .httpsCallable('subscribeToTopic')({ topic, token: this.token })
+      .pipe(tap(_ => this.makeToast(`subscribed to ${topic}`)))
+      .subscribe();
+  }
+
+  unsub(topic) {
+    this.fun
+      .httpsCallable('unsubscribeFromTopic')({ topic, token: this.token })
+      .pipe(tap(_ => this.makeToast(`unsubscribed from ${topic}`)))
+      .subscribe();
+  }
+}
